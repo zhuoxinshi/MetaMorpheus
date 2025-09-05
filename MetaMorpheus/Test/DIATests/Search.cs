4024ef2e52092ff0d12488d4ba08fcf1bffd4c70
Zhuoxin Shi <zshi94@wisc.edu>
2025-09-05 14:16:17 -0500

    promising?

--- MetaMorpheus/Test/DIATests/Search.cs
+++ MetaMorpheus/Test/DIATests/Search.cs
@@ -111,6 +111,7 @@ namespace Test.DIATests
             var features = new List<string> { "Correlation", "ApexRtDelta"};
             searchTask.CommonParameters.DIAparameters = new MLbasedDIAparameters(PseudoSearchScanType.DirectSearch, humanDb, false, ModelType.FastTree, features, 15, null, null, 0.2, 0.5, AnalysisType.MLbased, ms1XicConstructor, ms2XicConstructor, null, PseudoMs2ConstructionType.MzPeak) ;
             searchTask.RunTask(outputFolder, new List<DbForTask> { new DbForTask(humanDb, false) }, new List<string> { DIAfile }, "test");
+
         }
     }
 }
